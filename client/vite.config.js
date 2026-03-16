49111e81945436be57a7e985ac53f2d2e272479e
sakethpopuri <sakethpopuri2005@gmail.com>
2026-03-16 08:43:07 +0530

    Initial project commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    allowedHosts: [
+      "unlubricant-emmitt-unargumentatively.ngrok-free.dev"
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
@@ -13,3 +16,40 @@ export default defineConfig({
     },
   },
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
+
